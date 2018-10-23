98ab29e927c69af1b7763baff52765278e4fe291
Roman Bulinski <aboandrus@gmail.com>
2018-10-23 23:50:24 +0200

    Validate of files

--- validator.js
+++ validator.js
@@ -1,19 +1,55 @@
-function validation()
+
+
+function name_Is_Filled(){
+    if( document.getElementById("email").value.length > 0 &&
+        document.getElementById("subject_text").value.length > 0 ){
+            document.getElementById("subject_button").disabled = false;
+    }
+    return true;
+    
+}
+
+function email_Is_Filled(){
+    if( document.getElementById("name").value.length > 0 &&
+        document.getElementById("subject_text").value.length > 0 ){
+            document.getElementById("subject_button").disabled = false;
+    }
+    return true;
+    
+}
+
+function text_Is_Filled(){
+    if( document.getElementById("name").value.length > 0 &&
+        document.getElementById("email").value.length > 0 ){
+            document.getElementById("subject_button").disabled = false;
+    }
+    return true;
+    
+}
+
+
+function validation2()
 {
-    var form =  document.getElementById('info-form'),
-        rules = { // 1
-            'name' : /^[A-Z]{1}[a-z]{2,30}$/,
-            'email': /^[a-z0-9\._%-]+@[a-z0-9\.-]+\.[a-z]{2,4}$/i,
-            'subject': /^[a-z\s]{2,180}$/i,
-        };
+    var form =  document.getElementById('form'),
+
+    rules = { 
+        'name' : /^[A-Z]{1}[a-z]{2,30}$/,
+        'email': /^[a-z0-9\._%-]+@[a-z0-9\.-]+\.[a-z]{2,4}$/i,
+        'subject': /^[a-z\s]{5,180}$/i,
+    };
 
     for (var elem in rules) 
     {
-        if (form[elem])
         {
             if (!rules[elem].test(form[elem].value)) 
             {
-                alert('The Box " ' + elem + ' " is incorrectly filled.');
+                if( elem === 'name' )
+                {  alert('Corecct form of name is : Xxxxx !!! ');}
+                else if( elem === 'email' )
+                {  alert('Corecct form of email is : xxx@xxx.xxx !!!');}
+                else if( elem === 'subject' )
+                {  alert('Subject should has at least 3 letters !!!');}
+
                 form[elem].style.background = 'grey';
                 return false;
             }
@@ -26,7 +62,6 @@ function validation()
     alert('Your email has been successfully sent!');
     return true;
 }
-
 
 
 
